f015ebb0ba31f32bb9df8e76fbbbd5fb549e0821
ibu0125 <ibu01257248@gmail.com>
2024-09-03 17:46:03 +0900

    header

--- src/header.js
+++ src/header.js
@@ -1,10 +1,25 @@
 import React from "react";
-import "./index.css";
+import "./header.css";
 
 const Header = () => {
   return (
     <header className="header">
-      <div></div>
+      <div className="logo">My Website</div>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">about</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
+
+export default Header;
