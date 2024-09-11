f4ada0d9b3993ea6849095d835f84aa89f0892e4
ibu0125 <ibu01257248@gmail.com>
2024-09-11 22:55:19 +0900

    エラー修正

--- src/components/pages/MainContents.js
+++ src/components/pages/MainContents.js
@@ -118,7 +118,7 @@ function MainContents() {
                 <AnimatedDivBottom>
                   <img
                     className="profile-img"
-                    src={`${process.env.PUBLIC_URL}/images/landing-page.png`}
+                    src={`/react-basic-scrol/images/landing-page.png`}
                     alt="profileImage"
                   />
                 </AnimatedDivBottom>
